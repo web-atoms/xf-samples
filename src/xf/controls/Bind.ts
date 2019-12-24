26a4ce7749c2c1edecfb539874dab40078041ea2
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-12-24 21:34:27 +0530

    Binding fixed

--- src/xf/controls/Bind.ts
+++ src/xf/controls/Bind.ts
@@ -9,34 +9,26 @@ import { AtomXFControl } from "@web-atoms/core/dist/xf/controls/AtomXFControl";
 export type bindingFunction = (control: AtomXFControl) => any;
 
 function oneTime(name: string, b: Binding, control: AtomXFControl, e: any) {
-    b.path = name;
-    b.element = e;
     control.app.callLater(() => {
-        control.setLocalValue(b.element, b.path, b.sourcePath(control));
+        control.setLocalValue(e, name, b.sourcePath(control));
     });
 }
 
 function event(name: string, b: Binding, control: AtomXFControl, e: any) {
-    b.path = name;
-    b.element = e;
     control.app.callLater(() => {
-        control.bindEvent(b.element, b.path, b.sourcePath as any);
+        control.bindEvent(e, name, b.sourcePath as any);
     });
 }
 
 function oneWay(name: string, b: Binding, control: AtomXFControl, e: any) {
-    b.path = name;
-    b.element = e;
     control.app.callLater(() => {
-        control.bind(b.element, b.path, b.pathList , false, null);
+        control.bind(e, name, b.pathList , false, null);
     });
 }
 
 function twoWays(name: string, b: Binding, control: AtomXFControl, e: any) {
-    b.path = name;
-    b.element = e;
     control.app.callLater(() => {
-        control.bind(b.element, b.path, b.pathList, true, null);
+        control.bind(e, name, b.pathList, true, null);
     });
 }
 
@@ -64,15 +56,9 @@ export default class Binding {
 
     constructor(
         public readonly setupFunction: ((name: string, b: Binding, c: AtomXFControl, e: any) => void),
-        sourcePath: string | bindingFunction,
-        public element?: any,
-        public path?: string,
+        sourcePath: bindingFunction
         ) {
-        if (typeof sourcePath === "string") {
-            this.sourcePath = (b) => AtomBridge.instance.getValue(this.element, sourcePath);
-        } else {
-            this.sourcePath = sourcePath;
-        }
+        this.sourcePath = sourcePath;
 
         if (Array.isArray(this.sourcePath)) {
             this.pathList = this.sourcePath as any;
