10aeb708e1afe69d2cbd26460d85db5a83f90a0e
NS\Shubham <shubham.neurospeech@gmail.com>
2020-02-05 15:31:41 +0530

    changes

---
+++ src/samples/collection-view/CollectionViewSampleViewModel.ts
@@ -0,0 +1,7 @@
+import { AtomViewModel } from "@web-atoms/core/dist/view-model/AtomViewModel";
+
+export default class CollectionViewSampleViewModel extends AtomViewModel {
+    public async init(): Promise<any> {
+        
+    }
+}
