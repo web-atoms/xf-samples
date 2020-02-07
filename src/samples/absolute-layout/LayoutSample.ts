1b01a433eb18378b42313a8732c32239002382b5
basvaraj <basvaraj.neurospeech@gmail.com>
2020-02-07 14:45:50 +0530

    Stack-layout sample added

--- src/samples/absolute-layout/LayoutSample.ts
+++ src/samples/absolute-layout/LayoutSample.ts
@@ -1,9 +1,11 @@
 import MenuService from "../../service/menu-service/MenuService";
 import GridView from "../grid/GridView";
+import StackLayoutView from "../stack-layout/StackLayoutView";
 import AbsoluteLayoutView from "./AbsoluteLayoutView";
 
 export default function addLayoutSample(ms: MenuService) {
     const layout = ms.addGroup("Layout");
     layout.addLink("Absolute Layout", AbsoluteLayoutView);
+    layout.addLink("StackLayout", StackLayoutView);
     layout.addLink("Grid", GridView);
 }
