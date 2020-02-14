d13a33470fa2e58e8639a04a52e801ce9d303aa5
basvaraj <basvaraj.neurospeech@gmail.com>
2020-02-14 17:57:41 +0530

    layout image added

--- src/samples/layout/multiple-content/LayoutSample.ts
+++ src/samples/layout/multiple-content/LayoutSample.ts
@@ -1,7 +1,7 @@
 import MenuService from "../../../service/menu-service/MenuService";
 import AbsoluteLayoutView from "./absolute-layout/AbsoluteLayoutView";
 import FlexLayoutView from "./flex-layout/FlexLayoutView";
-import GridView from "./grid/GridView";
+import GridView from "./grid-layout/GridView";
 import StackLayoutView from "./stack-layout/StackLayoutView";
 
 export default function addLayoutSample(ms: MenuService) {
