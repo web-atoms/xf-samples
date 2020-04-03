852402359dd767c69858110508331ef23f91d0cd
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-04-03 20:17:08 +0530

    Added Custom Form

--- src/samples/form/custom/CustomForm.tsx
+++ src/samples/form/custom/CustomForm.tsx
@@ -4,20 +4,7 @@ import WA from "@web-atoms/xf-controls/dist/clr/WA";
 import X from "@web-atoms/xf-controls/dist/clr/X";
 import XF from "@web-atoms/xf-controls/dist/clr/XF";
 import AtomXFContentPage from "@web-atoms/xf-controls/dist/pages/AtomXFContentPage";
-import SimpleFormViewModel from "./SimpleFormViewModel";
-
-const NSXF = XNode.namespace("Xamarin.Forms", "Xamarin.Forms.Core");
-
-@NSXF("TemplateBinding")
-class TemplateBinding extends RootObject {
-
-    public path: string;
-
-}
-
-const XF2 = {
-    TemplateBinding
-};
+import SimpleFormViewModel from "../simple/SimpleFormViewModel";
 
 export default class SimpleForm extends AtomXFContentPage {
 
@@ -29,7 +16,25 @@ export default class SimpleForm extends AtomXFContentPage {
 
         this.render(<XF.ContentPage title="Simple Form" padding="10">
             <WA.AtomForm>
-                <WA.AtomField
+            <WA.AtomForm.fieldStyle>
+                <XF.ControlTemplate>
+                    <XF.Grid>
+                        <XF.Grid.rowDefinitions>
+                            <XF.RowDefinition height="Auto"/>
+                            <XF.RowDefinition/>
+                            <XF.RowDefinition height="Auto"/>
+                        </XF.Grid.rowDefinitions>
+                        <XF.Label
+                            text={X.TemplateBinding("Label")}
+                            textColor="Green"
+                            />
+                        <XF.ContentPresenter
+                            { ... XF.Grid.row(1)}
+                            />
+                    </XF.Grid>
+                </XF.ControlTemplate>
+            </WA.AtomForm.fieldStyle>
+            <WA.AtomField
                     label="Username:"
                     isRequired={true}
                     error={Bind.oneWay(() => this.viewModel.errorUsername)}>
