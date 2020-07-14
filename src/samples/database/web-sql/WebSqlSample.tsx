a8ba527aa7d99cb7dcbf2d3a80674f9038ed05de
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-07-14 14:55:21 +0530

    Added sqlite demo

--- src/samples/database/web-sql/WebSqlSample.tsx
+++ src/samples/database/web-sql/WebSqlSample.tsx
@@ -1,8 +1,25 @@
 import Bind from "@web-atoms/core/dist/core/Bind";
+import Colors from "@web-atoms/core/dist/core/Colors";
 import XNode from "@web-atoms/core/dist/core/XNode";
+import FontAwesomeRegular from "@web-atoms/font-awesome/dist/FontAwesomeRegular";
+import FontAwesomeSolid from "@web-atoms/font-awesome/dist/FontAwesomeSolid";
+import WA from "@web-atoms/xf-controls/dist/clr/WA";
 import XF from "@web-atoms/xf-controls/dist/clr/XF";
 import AtomXFContentPage from "@web-atoms/xf-controls/dist/pages/AtomXFContentPage";
 import WebSqlViewModel from "./WebSqlViewModel";
+
+function CommandButton(text: string, glyph, command: any) {
+    return <XF.Button
+        text={text}
+        command={command}>
+        <XF.Button.imageSource>
+            <XF.FontImageSource
+                fontFamily={FontAwesomeSolid}
+                glyph={glyph}
+                />
+        </XF.Button.imageSource>
+    </XF.Button>;
+}
 
 export default class WebSqlSample extends AtomXFContentPage {
 
@@ -12,19 +29,39 @@ export default class WebSqlSample extends AtomXFContentPage {
 
         this.viewModel = this.resolve(WebSqlViewModel);
 
-        this.render(<XF.ContentPage>
-
-            <XF.ListView itemsSource={Bind.oneWay(() => this.viewModel.rows )}>
-                <XF.ListView.itemTemplate>
-                    <XF.DataTemplate>
-                        <XF.TextCell
-                            text={Bind.oneWay((x) => x.data.data)}
-                            detail={Bind.oneWay((x) => x.data.id)}
+        this.render(<XF.ContentPage title="Sqlite Demo">
+            <XF.ContentPage.toolbarItems>
+                <WA.AtomToolbarItem
+                    command={Bind.event(() => this.viewModel.addRow())}>
+                    <XF.ToolbarItem.iconImageSource>
+                        <XF.FontImageSource
+                            fontFamily={FontAwesomeRegular}
+                            glyph={FontAwesomeRegular.plusSquare}
                             />
-                    </XF.DataTemplate>
-                </XF.ListView.itemTemplate>
-            </XF.ListView>
-
+                    </XF.ToolbarItem.iconImageSource>
+                </WA.AtomToolbarItem>
+            </XF.ContentPage.toolbarItems>
+            <XF.Grid rowDefinitions="*, auto">
+                <XF.ListView
+                    itemsSource={Bind.oneWay(() => this.viewModel.rows )}
+                    selectedItem={Bind.twoWays(() => this.viewModel.selectedRow)}>
+                    <XF.ListView.itemTemplate>
+                        <XF.DataTemplate>
+                            <XF.TextCell
+                                text={Bind.oneWay((x) => x.data.name)}
+                                detail={Bind.oneWay((x) => x.data.id)}
+                                />
+                        </XF.DataTemplate>
+                    </XF.ListView.itemTemplate>
+                </XF.ListView>
+                <XF.StackLayout
+                    isVisible={Bind.oneWay(() => this.viewModel.selectedRow ? true : false)}
+                    { ... XF.Grid.row(1) }
+                    orientation="Horizontal">
+                    {CommandButton("Modify", FontAwesomeSolid.edit, Bind.event(() => this.viewModel.edit()))}
+                    {CommandButton("Delete", FontAwesomeSolid.windowClose, Bind.event(() => this.viewModel.delete()))}
+                </XF.StackLayout>
+            </XF.Grid>
         </XF.ContentPage>);
     }
 
--- src/samples/database/web-sql/WebSqlViewModel.ts
+++ src/samples/database/web-sql/WebSqlViewModel.ts
@@ -1,32 +1,70 @@
+import { Inject } from "@web-atoms/core/dist/di/Inject";
+import { NavigationService } from "@web-atoms/core/dist/services/NavigationService";
+import Action from "@web-atoms/core/dist/view-model/Action";
 import { AtomViewModel } from "@web-atoms/core/dist/view-model/AtomViewModel";
 import Load from "@web-atoms/core/dist/view-model/Load";
-import sqlite, { ISqlDatabase } from "./Sqlite";
+import SqliteService, { SqliteConnection } from "@web-atoms/storage/dist/database/sqlite/SqliteService";
+import Query from "@web-atoms/storage/dist/query/Query";
+import ICustomer from "./model/ICustomer";
+import RowEditor from "./row-editor/RowEditor";
 
 export default class WebSqlViewModel extends AtomViewModel {
 
     public rows: any[];
 
-    private database: ISqlDatabase;
+    public selectedRow: any = null;
+
+    @Inject
+    private sqliteService: SqliteService;
+
+    @Inject
+    private navigationService: NavigationService;
+
+    private database: SqliteConnection;
 
     @Load({ init: true })
     public async loadDatabase() {
-        this.database = sqlite.openDatabase("db");
+        this.database = this.sqliteService.openDatabase("db", 1.1);
 
         await this.database.transactionAsync(async (tx) => {
             let r = await tx.executeSqlAsync(
-                "CREATE TABLE IF NOT EXISTS LOGS(id INTEGER PRIMARY KEY AUTOINCREMENT,data text)", []);
+                "CREATE TABLE IF NOT EXISTS Customers(id INTEGER PRIMARY KEY AUTOINCREMENT,name text)", []);
 
-            let a = await tx.executeSqlAsync("INSERT INTO LOGS (?)", ["a1"]);
-            // tslint:disable-next-line: no-console
-            console.log(`INSERT ID = ${a.insertId}`);
-            a = await tx.executeSqlAsync("INSERT INTO LOGS (?)", ["a1"]);
-            // tslint:disable-next-line: no-console
-            console.log(`INSERT ID = ${a.insertId}`);
-
-            r = await tx.executeSqlAsync("SELECT * FROM LOGS", []);
+            r = await tx.executeSqlAsync("SELECT * FROM Customers", []);
 
             this.rows = r.rows;
 
+        });
+    }
+
+    @Action()
+    public async addRow() {
+        const model = await this.navigationService.openPage(RowEditor, { model: {} }) as any;
+        await this.database.transactionAsync(async (tx) => {
+            await tx.insertAsync("Customers", model);
+            const r = await tx.executeSqlAsync("SELECT * FROM Customers", []);
+            this.rows = r.rows;
+        });
+    }
+
+    @Action()
+    public async edit() {
+        const model = await this.navigationService.openPage(RowEditor, { model: this.selectedRow }) as any;
+        await this.database.transactionAsync(async (tx) => {
+            const filter = Query.create ` id = ${this.selectedRow.id}`;
+            await tx.updateAsync("Customers", model, filter);
+            const r = await tx.executeSqlAsync("SELECT * FROM Customers", []);
+            this.rows = r.rows;
+        });
+    }
+
+    @Action({ confirm: "Are you sure you want to delete this?"})
+    public async delete() {
+        await this.database.transactionAsync(async (tx) => {
+            const filter = Query.create ` id = ${this.selectedRow.id}`;
+            await  tx.deleteAsync("Customers", filter);
+            const r = await tx.executeSqlAsync("SELECT * FROM Customers", []);
+            this.rows = r.rows;
         });
     }
 }
