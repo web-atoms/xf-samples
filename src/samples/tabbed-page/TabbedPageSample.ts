3166b18c199b092fa303606f793a91eb08b29dba
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-08-21 10:51:27 +0530

    wip

--- src/samples/tabbed-page/TabbedPageSample.ts
+++ src/samples/tabbed-page/TabbedPageSample.ts
@@ -1,5 +1,5 @@
 import MenuService from "../../service/menu-service/MenuService";
-import TabbedPageView from "./TabbedPageView";
+import TabbedPageView from "./TabbedPageView2";
 
 export default function addTabbedPage(ms: MenuService) {
     const tp = ms.addGroup("Tabbed Page");
