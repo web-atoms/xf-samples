76ca9bdc0dde7212e7ab9abb41c90a64391acebb
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-08-21 10:51:46 +0530

    Case fix

--- src/samples/tabbed-page/TabbedPageSample.ts
+++ src/samples/tabbed-page/TabbedPageSample.ts
@@ -1,5 +1,5 @@
 import MenuService from "../../service/menu-service/MenuService";
-import TabbedPageView from "./TabbedPageView2";
+import TabbedPageView from "./TabbedPageView";
 
 export default function addTabbedPage(ms: MenuService) {
     const tp = ms.addGroup("Tabbed Page");
