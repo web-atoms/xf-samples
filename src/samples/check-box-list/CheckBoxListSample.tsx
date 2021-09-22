621952383e4c1f31a1287322f95b752f9d584fe7
Akash Kava <ackava@gmail.com>
2021-09-22 18:24:48 +0530

    wip

--- src/samples/check-box-list/CheckBoxListSample.tsx
+++ src/samples/check-box-list/CheckBoxListSample.tsx
@@ -1,9 +1,11 @@
 import Bind from "@web-atoms/core/dist/core/Bind";
 import { BindableProperty } from "@web-atoms/core/dist/core/BindableProperty";
 import XNode from "@web-atoms/core/dist/core/XNode";
+import FontAwesomeSolid from "@web-atoms/font-awesome/dist/FontAwesomeSolid";
 import WA from "@web-atoms/xf-controls/dist/clr/WA";
 import XF from "@web-atoms/xf-controls/dist/clr/XF";
 import AtomXFContentPage from "@web-atoms/xf-controls/dist/pages/AtomXFContentPage";
+import SwipeCollectionViewSample from "../collection-view/swipe/SwipeCollectionViewSample";
 
 const options = [
     "Audio",
@@ -23,8 +25,8 @@ export default class CheckBoxListSample extends AtomXFContentPage {
 
         this.options = [];
 
-        this.render(<XF.ContentPage>
-            <XF.Grid>
+        this.render(<XF.ContentPage title="Check Box List">
+            <XF.Grid padding={5}>
                 <XF.ScrollView>
                     <WA.AtomForm>
                         <WA.AtomField
@@ -42,6 +44,44 @@ export default class CheckBoxListSample extends AtomXFContentPage {
                                     : "...")}
                                 />
                         </WA.AtomField>
+
+                        <XF.StackLayout background={XF.SolidColorBrush.red}>
+                            <XF.Image>
+                                <XF.Image.source>
+                                    <XF.FontImageSource
+                                            fontFamily={FontAwesomeSolid}
+                                            glyph={FontAwesomeSolid.search}
+                                            color={XF.Color.darkOrange}
+                                            />
+                                </XF.Image.source>
+                            </XF.Image>
+                        </XF.StackLayout>
+
+                        <WA.AtomExpander
+                            icon={ Object.assign( new XF.FontImageSource(), {
+                                fontFamily: FontAwesomeSolid,
+                                glyph: FontAwesomeSolid.search,
+                                color: XF.Color.darkOrange
+                            }) }
+                            background={XF.SolidColorBrush.green}>
+                            {/* <WA.AtomExpander.icon>
+                                <XF.FontImageSource
+                                    fontFamily={FontAwesomeSolid}
+                                    glyph={FontAwesomeSolid.search}
+                                    color={XF.Color.darkOrange}
+                                    size={10}
+                                    />
+                            </WA.AtomExpander.icon> */}
+                            <XF.Label
+                                text="Demo"
+                                />
+                            <WA.AtomExpander.detail>
+                                <XF.Label
+                                    text="This is detail"
+                                    />
+                            </WA.AtomExpander.detail>
+                        </WA.AtomExpander>
+
                     </WA.AtomForm>
                 </XF.ScrollView>
             </XF.Grid>
